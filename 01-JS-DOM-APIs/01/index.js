18a69700698c7092f0a36d71f4f1cad4f911da8e
Gabyfinn <gabrieluzzante@gmail.com>
2023-02-22 12:23:41 -0300

    Agregando API con promesas

--- 01-JS-DOM-APIs/01/index.js
+++ 01-JS-DOM-APIs/01/index.js
@@ -1,17 +1,103 @@
+
 function fadeIn() {
-  console.log("entre")
   let element = document.getElementById('hide');
-  console.log(element.style.display)
-  // if (element.style.display == '') {
-  //   setTimeout(() => {
-  //     element.style.display = "block";
-  //   }, "1000")
-    element.style.transition = "opacity 5s linear 2s";
-    element.style.opacity = 1;
-  
-  console.log(element.style.display)
+  element.style.transition = "opacity 5s linear 2s";
+  element.style.opacity = 1;
   // element.style.transition = "opacity 5s linear 2s";
 
   // element.style.transitionDelay = '2s';
   // element.style.opacity = 1;
+}
+
+function click4(msg) {
+  console.log(msg)
+  console.log("Realice un click");
+  alert("hiciste click en el boton");
+}
+
+
+
+// function reqListener(event) {
+//   console.log("La transferencia se a completado.");
+//   console.log(event);
+//   console.log(this.responseText);
+//   console.log(this.responseText.value);
+// }
+
+// function updateProgress(oEvent) {
+//   if (oEvent.lengthComputable) {
+//     var percentComplete = oEvent.loaded / oEvent.total * 100;
+//     console.log(percentComplete)
+//   }
+// }
+
+// function transferFailed() {
+
+// }
+
+// function transferCanceled() {
+
+// }
+
+async function chuckApiCall() {
+  // let conf = { method: "GET", url: "https://api.chucknorris.io/jokes/random" }
+  try{
+    
+    let conf = { method: "GET", url: "http://api.icndb.com/jokes/random" }
+    const response = await ajaxCall(conf);
+    let element = document.getElementById('hide');
+    element.innerHTML = response.value;
+  }
+  catch(error){
+    console.log(error)
+  }
+
+}
+
+// fetch('https://api.chucknorris.io/jokes/random')
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+// console.log(oReq.responseText);
+
+
+// Crear una función reutilizable para realizar llamadas AJAX. Esta función debe aceptar un objeto de configuración y devolver una promesa ES6.
+
+async function ajaxCall(conf) {
+  // oReq.addEventListener("progress", updateProgress);
+  // oReq.addEventListener("load", reqListener);
+  // oReq.addEventListener("error", transferFailed);
+  // oReq.addEventListener("abort", transferCanceled);
+  return new Promise(function (resolve, reject) {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open(conf.method, conf.url);
+    let response;
+    httpRequest.onload = (e) => {
+      if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+          console.log(httpRequest.responseText);
+          response = JSON.parse(httpRequest.responseText);
+          console.log(`La respuesta es ${response.value}`);
+          resolve(response);
+        }
+        else {
+          console.log(httpRequest.statusText)
+          console.log(httpRequest.responseText)
+          reject({
+            status: this.status,
+            statusText: httpRequest.statusText
+          })
+        }
+      }
+    };
+    httpRequest.onerror = (e) => {
+      console.log(httpRequest.responseText)
+      reject({
+        status: this.status,
+        statusText: httpRequest.statusText
+      })
+    }
+    httpRequest.send(null);
+  }
+  )
+  
 }
